b060b3059fe070eefc22eea555b91535e0654a5e
ShaikAbdulRazzaaq <shaik.11715485@lpu.in>
2020-05-06 00:11:04 +0530

    Cleaning Code

--- app/src/main/java/com/projects/popularmovies/DetailsActivity.java
+++ app/src/main/java/com/projects/popularmovies/DetailsActivity.java
@@ -16,8 +16,9 @@ public class DetailsActivity extends AppCompatActivity {
     private TextView overview;
     private TextView language;
     private ImageView thumbnailDetailActivity;
-    private static final String PosterUrl="https://image.tmdb.org/t/p/w500";
+    private static final String PosterUrl = "https://image.tmdb.org/t/p/w500";
     private static final String TAG = "DetailsActivity";
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,11 +29,11 @@ public class DetailsActivity extends AppCompatActivity {
         releaseDate = findViewById(R.id.releaseDate);
         rating = findViewById(R.id.rating);
         overview = findViewById(R.id.overview);
-        language=findViewById(R.id.language);
+        language = findViewById(R.id.language);
         thumbnailDetailActivity = findViewById(R.id.thumbnailDetailActivity);
         Bundle b;
         b = getIntent().getExtras();
-        if(b!=null) {
+        if (b != null) {
             Result result = b.getParcelable("Movies");
             assert result != null;
             TitleDetailActivity.setText(result.getTitle());
@@ -41,9 +42,9 @@ public class DetailsActivity extends AppCompatActivity {
             releaseDate.setText(new StringBuilder("Release Date: ").append(result.getReleaseDate()));
             language.setText(new StringBuilder("Language: ").append(result.getOriginalLanguage()));
             Picasso.get()
-                    .load(PosterUrl+result.getBackdropPath())
+                    .load(PosterUrl + result.getBackdropPath())
                     .into(thumbnailDetailActivity);
-            Log.e(TAG, "onCreate: "+PosterUrl+result.getBackdropPath() );
+            Log.e(TAG, "onCreate: " + PosterUrl + result.getBackdropPath());
         }
     }
 }
